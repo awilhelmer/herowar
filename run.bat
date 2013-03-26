effc1473008b6e0cc0076b9e2e32d5a3b3f5a44f
awilhelmer <awilhelmer@gmx.de>
2013-03-26 20:10:56 +0100

    run Script with clean ...

--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
-play ~run
+@echo off
+call play clean
+@echo ########################## Resources cleaned... Starting Server now... ########################## 
+call play ~run
+@echo ##########################         Server has stopped...               ##########################
 pause
