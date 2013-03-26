e750f2dc45bb76050006eda20f09a799dccb4736
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-03-26 19:44:42 +0100

    improved build script

--- project/Build.scala
+++ project/Build.scala
@@ -2,125 +2,7 @@ import sbt._
 import Keys._
 import play.Project._
 
-object ApplicationBuild extends Build {
-
-	val concaterOptions = SettingKey[Seq[String]]("play-concater-options")
-    val concaterEntryPoints = SettingKey[PathFinder]("play-concater-entry-points")
-    val handlebarsOptions = SettingKey[Seq[String]]("play-handlebars-options")
-    val handlebarsEntryPoints = SettingKey[PathFinder]("play-handlebars-entry-points")
-
-	def LogTask() =
-	  	(concaterEntryPoints, sourceDirectory in Compile, cacheDirectory, streams) map { (files, src, cache, s) =>
-	  		s.log.info("Stream")
-	  		val watch = ("*.coffee")
-	  		val cacheFile = cache / "Test"    
-	  		// val currentInfos = watch(src).get.map(f => f -> FileInfo.lastModified(f)).toMap
-	  		val (previousRelation, previousInfo) = Sync.readInfo(cacheFile)(FileInfo.lastModified.format)                                                                                                                                                                                                                          
-	  		previousRelation._2s.toSeq
-	  	}
-
-	def JavascriptConcater() = {
-		println("JavascriptConcater")
-		val concater = new JavascriptConcater()
-		MyAssetsCompiler("coffeescript",
-        	(_ ** "*.coffee"),
-        	coffeescriptEntryPoints,
-        	{ (name, min) => "javascripts/" + name + ".pre" + (if (min) ".min.js" else ".js") },
-        	{ (file, options) =>
-        		println(file)
-          		val (jsSource, dependencies) = concater.compileDir(file, options)
-          		import scala.util.control.Exception._
-          		val minified = catching(classOf[CompilationException])
-            		.opt(play.core.jscompile.JavascriptCompiler.minify(jsSource, Some(file.getName())))
-          		(jsSource, minified, dependencies)
-        	},
-        	concaterOptions
-      	)
-	}
-	
-	def HandlebarsCompiler() = {
-		println("HandlebarsCompiler")
-		MyAssetsCompiler("handlebars",
-        	(_ ** "*.tmpl"),
-        	handlebarsEntryPoints,
-        	{ (name, min) => "javascripts/" + name + ".pre" + (if (min) ".min.js" else ".js") },
-        	{ (file, options) =>
-        		println(file)
-        		val dependencies = Seq.newBuilder[File]
-          		("Test", None, dependencies.result)
-        	},
-        	handlebarsOptions
-      	)
-	}
-	
-	// Name: name of the compiler
-  // files: the function to find files to compile from the assets directory
-  // naming: how to name the generated file from the original file and whether it should be minified or not
-  // compile: compile the file and return the compiled sources, the minified source (if relevant) and the list of dependencies                                                                                                                                                                                               
-  def MyAssetsCompiler(
-    name: String,  
-    watch: File => PathFinder, 
-    filesSetting: sbt.SettingKey[PathFinder],
-    naming: (String, Boolean) => String,
-    compile: (File, Seq[String]) => (String, Option[String], Seq[File]),
-    optionsSettings: sbt.SettingKey[Seq[String]]) = {
-    
-    println("MyAssetsCompiler")
-    println(sourceDirectory in Compile)
-    println(resourceManaged in Compile)
-    println(cacheDirectory)
-    println(optionsSettings)
-    println(filesSetting)
-    println(classDirectory in Compile)
-    
-    (streams, sourceDirectory in Compile, resourceManaged in Compile, cacheDirectory, optionsSettings, filesSetting, classDirectory in Compile) map { (s, src, resources, cache, options, files, classDirectory) =>                                                                                                                                                                 
-
-	  s.log.info("MyAssetsCompiler2")
-
-      import java.io._
-      
-      println("MyAssetsCompiler3")
-
-      val cacheFile = cache / name    
-      val currentInfos = watch(src).get.map(f => f -> FileInfo.lastModified(f)).toMap
-      val (previousRelation, previousInfo) = Sync.readInfo(cacheFile)(FileInfo.lastModified.format)                                                                                                                                                                                                                          
-
-      if (previousInfo != currentInfos) {                                                                                                                                                                                                                                                                                    
-
-        // Delete previous generated files
-        previousRelation._2s.foreach(IO.delete)                                                                                                                                                                                                                                                                              
-
-        val generated = (files x relativeTo(Seq(src / "assets" ))).flatMap {                                                                                                                                                                                                                                                  
-          case (sourceFile, name) => { 
-            val (debug, min, dependencies) = compile(sourceFile, options) 
-            val out = new File(resources, "public/" + naming(name, false))
-            val outMin = new File(resources, "public/" + naming(name, true))                                                                                                                                                                                                                                                 
-            IO.write(out, debug)
-            (dependencies ++ Seq(sourceFile)).toSet[File].map(_ -> out) ++ min.map { minified =>
-                val outMin = new File(resources, "public/" + naming(name, true))
-                IO.write(outMin, minified)
-                (dependencies ++ Seq(sourceFile)).map(_ -> outMin)
-            }.getOrElse(Nil)
-          }
-        }
-        
-        Sync.writeInfo(
-            cacheFile,         
-            Relation.empty[File, File] ++ generated,
-            currentInfos
-        )(FileInfo.lastModified.format)                                                                                                                                                                                                                                                                        
-
-        generated.map(_._2).distinct.toList
-      } else {
-
-        // Return previously generated files
-        previousRelation._2s.toSeq                                                                                                                                                                                                                                                                                           
-
-      }
-      
-      }
-
-    }: sbt.Project.Initialize[sbt.Task[Seq[java.io.File]]]  
+object ApplicationBuild extends Build with CustomAssetsCompiler {
 
 	val appName         = "herowar"
 	val appVersion      = "1.0-SNAPSHOT"
@@ -134,19 +16,19 @@ object ApplicationBuild extends Build {
   	)
 
   	val main = play.Project(appName, appVersion, appDependencies).settings(
-  		concaterEntryPoints <<= (sourceDirectory in Compile)(base => base / "assets" / "javascripts"),
-      	concaterOptions := Seq.empty[String],
+  		coffeescriptEntryPoints <<= (sourceDirectory in Compile)(base => base / "assets" ** "*.coffee"),
+      	coffeescriptSettings := Seq.empty[String],
       	
-      	handlebarsEntryPoints <<= (sourceDirectory in Compile)(base => base / "assets" / "templates"),
-      	handlebarsOptions := Seq.empty[String]
+      	handlebarsEntryPoints <<= (sourceDirectory in Compile)(base => base / "assets"  ** "*.tmpl"),
+      	handlebarsSettings := Seq.empty[String],
       	
-      	// Override resource generators and add less and coffescript compiler
-      	//resourceGenerators in Compile <<= LessCompiler(Seq(_)),
-      	//resourceGenerators in Compile <+= JavascriptConcater,
-      	//resourceGenerators in Compile <+= HandlebarsCompiler,
-      	//resourceGenerators in Compile <+= LogTask
+      	// Override resource generators and add custom compiler
+      	resourceGenerators in Compile <<= LessCompiler(Seq(_)),
+      	resourceGenerators in Compile <+= CustomCoffeescriptCompiler,
+      	resourceGenerators in Compile <+= CustomHandlebarsCompiler,
+      	
+      	concatSettings,
+      	compile in Compile <<= (concatTask).dependsOn(compile in Compile)
   	)
-  	
-  	println("Test")
 
 }
---
+++ project/CustomAssetsCompiler.scala
@@ -0,0 +1,115 @@
+package sbt
+
+import Keys._
+import PlayKeys._
+import PlayExceptions._
+import play.api.PlayException
+
+// ----- Assets
+trait CustomAssetsCompiler {
+
+	val coffeescriptSettings = SettingKey[Seq[String]]("coffeescript-settings")
+	val coffeescriptEntryPoints = SettingKey[PathFinder]("coffeescript-entry-points")
+	val handlebarsSettings = SettingKey[Seq[String]]("handlebars-settings")
+	val handlebarsEntryPoints = SettingKey[PathFinder]("handlebars-entry-points")
+	
+	val concatFolders = SettingKey[Seq[File]]("concat-folders")
+	val concatTask = TaskKey[sbt.inc.Analysis]("concat")
+	val concatSettings = concatTask := {
+		println("Concat Task runs now !!!!")
+		//(concatFolders map { f => f -> println(f.getName()) })
+		sbt.inc.Analysis.Empty
+	}
+
+	def AdvancedAssetsCompiler(
+		name: String,
+		baseFolder: String,
+		level: Int,
+		watch: File => PathFinder, 
+		filesSetting: sbt.SettingKey[PathFinder],
+		naming: (String, Boolean) => String,
+		compile: (File, Seq[String]) => (String, Option[String], Seq[File]),
+		optionsSettings: sbt.SettingKey[Seq[String]]) =     
+		(state, sourceDirectory in Compile, resourceManaged in Compile, cacheDirectory, optionsSettings, filesSetting, classDirectory in Compile) map { (state, src, resources, cache, options, files, classDirectory) =>                                                                                                                                                                 
+	
+			state.log.info("AssetsCompiler: " + name)
+		
+			import java.io._
+	      
+			val cacheFile = cache / name    
+	    	val currentInfos = watch(src).get.map(f => f -> FileInfo.lastModified(f)).toMap
+	    	val (previousRelation, previousInfo) = Sync.readInfo(cacheFile)(FileInfo.lastModified.format)  
+	    	
+	    	val concatFiles = false
+	    	val concatPath = ""                                                                                                                                                                                                                        
+	
+	    	if (previousInfo != currentInfos) {                                                                                                                                                                                                                                                                                    
+	
+	        	// Delete previous generated files
+	        	previousRelation._2s.foreach(IO.delete)                                                                                                                                                                                                                                                                              
+	
+	        	val generated = (files x relativeTo(Seq(src / "assets" ))).flatMap {                                                                                                                                                                                                                                                  
+	          		case (sourceFile, name) => {
+	          			if (baseFolder ne null) {
+	          				val sourcePath = sourceFile.getAbsolutePath
+	          				val relativePath = sourcePath.substring(sourcePath.lastIndexOf(baseFolder) + baseFolder.length() + 1)
+	          				var foundLevel = relativePath.count(_ == '\\')	     
+	          				if (foundLevel >= level) {
+	          					val parentPath = sourcePath.substring(0, sourcePath.lastIndexOf('\\'))
+	          					state.log.info("Concat this folder: " + parentPath)
+	          					concatFolders += new File(parentPath)
+	          				}
+//	          				state.log.info(relativePath + " - found level: " + foundLevel)
+	          			}
+	            		val (debug, min, dependencies) = compile(sourceFile, options) 
+	            		val out = new File(resources, "public/" + naming(name, false))
+	            		val outMin = new File(resources, "public/" + naming(name, true))                                                                                                                                                                                                                                                 
+	            		IO.write(out, debug)
+	            		(dependencies ++ Seq(sourceFile)).toSet[File].map(_ -> out) ++ min.map { minified =>
+	                		val outMin = new File(resources, "public/" + naming(name, true))
+	                		IO.write(outMin, minified)
+	                		(dependencies ++ Seq(sourceFile)).map(_ -> outMin)
+	            		}.getOrElse(Nil)
+	          		}
+	        	}
+	        
+	        	Sync.writeInfo(
+	            	cacheFile,         
+	            	Relation.empty[File, File] ++ generated,
+	            	currentInfos
+	        	)(FileInfo.lastModified.format)                                                                                                                                                                                                                                                                        
+	
+	        	generated.map(_._2).distinct.toList
+	      	} else {
+	
+	        	// Return previously generated files
+	        	previousRelation._2s.toSeq
+	      
+	      	}
+	
+		}: sbt.Project.Initialize[sbt.Task[Seq[java.io.File]]]	
+
+	def CustomCoffeescriptCompiler() = {
+		AdvancedAssetsCompiler("coffeescript", "javascripts", 1, (_ ** "*.coffee"), coffeescriptEntryPoints,
+        	{ (name, min) => name.replace(".coffee", if (min) ".min.js" else ".js") },
+        	{ (coffeeFile, options) =>
+        		import scala.util.control.Exception._
+			    val jsSource = play.core.coffeescript.CoffeescriptCompiler.compile(coffeeFile, options)
+			    val minified = catching(classOf[CompilationException]).opt(play.core.jscompile.JavascriptCompiler.minify(jsSource, Some(coffeeFile.getName())))
+			    (jsSource, minified, Seq(coffeeFile))
+        	}, coffeescriptSettings
+      	)
+	}
+	
+	def CustomHandlebarsCompiler() = {
+		AdvancedAssetsCompiler("handlebars",	"templates", 0,	(_ ** "*.tmpl"), handlebarsEntryPoints,
+        	{ (name, min) => "javascripts/" + name + ".pre" + (if (min) ".min.js" else ".js") },
+        	{ (file, options) =>
+        		//println(file)
+        		val dependencies = Seq.newBuilder[File]
+          		("Test", None, dependencies.result)
+        	}, handlebarsSettings
+      	)
+	}
+		
+}
