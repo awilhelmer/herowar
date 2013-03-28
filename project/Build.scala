68ad81dbb5c09f143754a4b878406dcd114df66f
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-03-28 23:58:27 +0100

    fixed compile issue

--- project/Build.scala
+++ project/Build.scala
@@ -29,8 +29,9 @@ object ApplicationBuild extends Build with CustomAssetsCompiler with JavascriptT
     handlebarsEntryPoints <<= (sourceDirectory in Compile)(base => base / "assets" ** "*.tmpl"),
     handlebarsSettings := Seq.empty[String],
     resourceGenerators in Compile <+= HandlebarsCompiler(handlebars = handlebarsJS),
-    //resources in Compile ~= )
-      resources in Compile ~= (classDirectory in Compile, resources) { (classDirectory, resources) => transformResources(classDirectory, resources) }
+    //resources in Compile ~= tranformResources
+      //resources in Compile ~= (classDirectory in Compile, Seq.empty[java.io.File])(transformResources)
+      resources in Compile <<= (classDirectory in Compile, resources in Compile) map transformResources
     )
   ////////// DEPENDENCIES //////////
 
