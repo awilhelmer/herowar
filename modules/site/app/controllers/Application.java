452caae800a532ef687cac5189883e097aeb9c7c
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-03-29 04:54:39 +0100

    load proper javascript files in main template

--- modules/site/app/controllers/Application.java
+++ modules/site/app/controllers/Application.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import info.BuildInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -14,6 +19,15 @@ public class Application extends Controller {
 
   public static Result index() {
     return ok(index.render());
+  }
+  
+  public static List<String> getJavascripts() {
+    // TODO: this should be cached somewhere ...
+    List<String> jsFiles = new ArrayList<String>();
+    jsFiles.add("javascripts/ps" + BuildInfo.cacheNumber() + ".js");
+    jsFiles.add("javascripts/pt" + BuildInfo.cacheNumber() + ".js");
+    jsFiles.add("javascripts/pv" + BuildInfo.cacheNumber() + ".js");
+    return jsFiles;
   }
 
 }
