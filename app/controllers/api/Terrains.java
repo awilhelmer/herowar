ab90ae71c7e0c590adeba9aba4c2329647863b8e
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-04-20 18:13:36 +0200

    added api for terrains

---
+++ app/controllers/api/Terrains.java
@@ -0,0 +1,48 @@
+package controllers.api;
+
+import static play.libs.Json.toJson;
+import models.entity.game.Terrain;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.mvc.Result;
+
+/**
+ * The Terrains controller handle api requests for the Terrain model.
+ * 
+ * @author Sebastian Sachtleben
+ */
+public class Terrains extends BaseAPI<Long, Terrain> {
+
+  private Terrains() {
+    super(Long.class, Terrain.class);
+  }
+
+  public static final Terrains instance = new Terrains();
+
+  @Transactional
+  public static Result list() {
+    return instance.listAll();
+  }
+
+  @Transactional
+  public static Result show(Long id) {
+    return instance.showEntry(id);
+  }
+
+  @Transactional
+  public static Result update(Long id) {
+    Terrain terrain = instance.findUnique(id);
+    terrain = JPA.em().merge(terrain);
+    return ok(toJson(terrain));
+  }
+
+  @Transactional
+  public static Result delete(Long id) {
+    return instance.deleteEntry(id);
+  }
+
+  @Transactional
+  public static Result add() {
+    return instance.addEntry();
+  }
+}
