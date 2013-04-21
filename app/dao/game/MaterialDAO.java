e2957b27df93b6bcf5caade9ae4a494fc1dd899d
awilhelmer <awilhelmer@gmx.de>
2013-04-21 22:49:25 +0200

    Refactoring Material handling

--- app/dao/game/MaterialDAO.java
+++ app/dao/game/MaterialDAO.java
@@ -1,6 +1,10 @@
 package dao.game;
 
+import java.util.HashMap;
+import java.util.List;
+
 import models.entity.game.Material;
+import play.db.jpa.JPA;
 import dao.BaseDAO;
 
 public class MaterialDAO extends BaseDAO<Long, Material> {
@@ -14,9 +18,25 @@ public class MaterialDAO extends BaseDAO<Long, Material> {
   public static Material getMaterialbyId(Long id) {
     return instance.findUnique(id);
   }
-  
+
   public static Material mergeMaterial(Material material) {
     return instance.merge(material);
+  }
+
+  public static java.util.Map<Integer, Material> mapAndSave(List<Material> materials) {
+    java.util.Map<Integer, Material> result = new HashMap<Integer, Material>();
+    for (int i = 0; i < materials.size(); i++) {
+      Material mat = materials.get(i);
+      Material dbMat = MaterialDAO.getMaterialbyId(mat.getId());
+      if (dbMat == null) {
+        dbMat = mat;
+        JPA.em().persist(dbMat);
+      } else {
+        dbMat = MaterialDAO.mergeMaterial(mat);
+      }
+      result.put(i, dbMat);
+    }
+    return result;
   }
 
 }
