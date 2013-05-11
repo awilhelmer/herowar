0f1d17eb504b6922d8c7b33a8f7fd13f47ba12ff
awilhelmer <awilhelmer@gmx.de>
2013-05-11 18:41:02 +0200

    Prework for unithandling

---
+++ app/game/models/UnitModel.java
@@ -0,0 +1,33 @@
+package game.models;
+
+import models.entity.game.Path;
+import models.entity.game.Waypoint;
+
+public class UnitModel extends BaseModel {
+  private static final long serialVersionUID = 1L;
+
+  private Path activePath;
+
+  private Waypoint activeWaypoint;
+
+  public UnitModel(Long id, Long dbId) {
+    super(id, dbId);
+  }
+
+  public Path getActivePath() {
+    return activePath;
+  }
+
+  public void setActivePath(Path activePath) {
+    this.activePath = activePath;
+  }
+
+  public Waypoint getActiveWaypoint() {
+    return activeWaypoint;
+  }
+
+  public void setActiveWaypoint(Waypoint activeWaypoint) {
+    this.activeWaypoint = activeWaypoint;
+  }
+
+}
