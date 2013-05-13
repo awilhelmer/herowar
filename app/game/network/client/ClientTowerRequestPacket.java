5bfea87099142d460b625907f8e44d63f8492eb7
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-05-13 20:38:29 +0200

    improved tower request packet

--- app/game/network/client/ClientTowerRequestPacket.java
+++ app/game/network/client/ClientTowerRequestPacket.java
@@ -1,6 +1,10 @@
 package game.network.client;
 
+import models.entity.game.Vector3;
+
 import org.webbitserver.WebSocketConnection;
+
+import play.Logger;
 
 import game.network.BasePacket;
 import game.network.InputPacket;
@@ -15,10 +19,34 @@ import game.network.handler.WebSocketHandler;
 @SuppressWarnings("serial")
 public class ClientTowerRequestPacket extends BasePacket implements InputPacket {
 
+  private static final Logger.ALogger log = Logger.of(ClientTowerRequestPacket.class);
+  
+  private Long id;
+  private Vector3 position;
+  
 	@Override
 	public void process(PacketHandler packetHandler, WebSocketHandler socketHandler, WebSocketConnection connection) {
-		// TODO Auto-generated method stub
-		
+	  log.info("Process " + this.toString());
 	}
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Vector3 getPosition() {
+    return position;
+  }
+
+  public void setPosition(Vector3 position) {
+    this.position = position;
+  }
+
+  @Override
+  public String toString() {
+    return "ClientTowerRequestPacket [type=" + type + ", createdTime=" + createdTime + ", id=" + id + ", position=" + position + "]";
+  }
 }
