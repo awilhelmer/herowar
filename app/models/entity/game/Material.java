39aa03bc57feb25b6c7618161fe5f75f3750fa83
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-06-06 20:16:21 +0200

    changed material entity

--- app/models/entity/game/Material.java
+++ app/models/entity/game/Material.java
@@ -6,6 +6,7 @@ import game.json.StringArray;
 import game.json.StringArray.ArrayType;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -27,21 +28,31 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 
+/**
+ * The mesh materials for the game models.
+ * 
+ * @author Alexander Wilhelmer
+ * @author Sebastian Sachtleben
+ */
 @Entity
 @Table(name = "material", uniqueConstraints = @UniqueConstraint(columnNames = "dbgname"))
 @JsonDeserialize(using = MaterialDeserializer.class)
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@SuppressWarnings("serial")
 public class Material implements Serializable {
-  private static final long serialVersionUID = 1651915135235L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @NotNull
+  @Column(length = 100)
   private String name;
 
-  private String color;
+  private Integer sortIndex;
+
+  private String color = "#ffffff";
+
   // They are for model imports
   @JsonFieldName(name = "DbgColor")
   private Integer dbgColor;
@@ -51,43 +62,64 @@ public class Material implements Serializable {
   @Column(name = "dbgname")
   private String dbgName;
 
-  private String blending;
-  private Boolean depthTest;
-  private Boolean depthWrite;
+  private String blending = "NormalBlending";
+  private String shading = "SmoothShading";
 
-  private Integer sortIndex;
+  private Boolean depthTest = Boolean.FALSE;
+  private Boolean depthWrite = Boolean.FALSE;
+
+  private Boolean transparent = Boolean.FALSE;
+  private Boolean vertexColors = Boolean.FALSE;
+
+  @StringArray(type = ArrayType.DOUBLE)
+  private String colorDiffuse = "[1.0, 1.0, 1.0]";
+  @StringArray(type = ArrayType.DOUBLE)
+  private String colorSpecular = "[0.0, 0.0, 0.0]";
+  @StringArray(type = ArrayType.DOUBLE)
+  private String colorAmbient = "[1.0, 1.0, 1.0]";
+
+  @Column(scale = 3)
+  private Float transparency = 1.0F;
+  private Integer specularCoef = 30;
+
+  @Column(precision = 2)
+  private Float opacity = 1.0F;
+
+  private String mapDiffuse = null;
+  private String mapLight = null;
+  private String mapBump = null;
+  private String mapNormal = null;
+  private String mapSpecular = null;
 
   @StringArray(type = ArrayType.STRING)
-  private String mapDiffuseWrap;
-  private String shading;
-  private Integer specularCoef;
+  private String mapDiffuseWrap = null;
+  @StringArray(type = ArrayType.STRING)
+  private String mapLightWrap = null;
+  @StringArray(type = ArrayType.STRING)
+  private String mapBumpWrap = null;
+  @StringArray(type = ArrayType.STRING)
+  private String mapNormalWrap = null;
+  @StringArray(type = ArrayType.STRING)
+  private String mapSpecularWrap = null;
+
   @Column(scale = 3)
-  private Float transparency;
-  private Boolean vertexColors;
-  private Boolean transparent;
-
-  @StringArray(type = ArrayType.DOUBLE)
-  private String colorAmbient;
-  @StringArray(type = ArrayType.DOUBLE)
-  private String colorDiffuse;
-  @StringArray(type = ArrayType.DOUBLE)
-  private String colorSpecular;
-  private String mapDiffuse;
+  private Float mapBumpScale = 1.0F;
 
   @Lob
   @StringArray(type = ArrayType.OBJECT)
-  private String attributes;
+  private String attributes = null;
   @Lob
   @StringArray(type = ArrayType.OBJECT)
-  private String uniforms;
+  private String uniforms = null;
 
   @Lob
   @StringArray(type = ArrayType.SHADER)
-  private String vertexShader;
+  private String vertexShader = null;
 
   @Lob
   @StringArray(type = ArrayType.SHADER)
-  private String fragmentShader;
+  private String fragmentShader = null;
+
   @Transient
   private Long materialId;
 
@@ -96,14 +128,13 @@ public class Material implements Serializable {
 
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "allMaterials")
   @JsonIgnore
-  private Set<Map> maps;
+  private Set<Map> maps = new HashSet<Map>();
 
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "id.material")
   @JsonIgnore
-  private Set<GeoMaterial> geoMaterials;
+  private Set<GeoMaterial> geoMaterials = new HashSet<GeoMaterial>();
 
-  @Column(precision = 2)
-  private Float opacity;
+  // / GETTER && SETTER ///
 
   public Long getId() {
     return id;
@@ -113,7 +144,6 @@ public class Material implements Serializable {
     this.id = id;
   }
 
-  @Column(name = "name", length = 100)
   public String getName() {
     return name;
   }
@@ -122,52 +152,20 @@ public class Material implements Serializable {
     this.name = name;
   }
 
+  public Integer getSortIndex() {
+    return sortIndex;
+  }
+
+  public void setSortIndex(Integer sortIndex) {
+    this.sortIndex = sortIndex;
+  }
+
   public String getColor() {
     return color;
   }
 
   public void setColor(String color) {
     this.color = color;
-  }
-
-  public Boolean getTransparent() {
-    return transparent;
-  }
-
-  public void setTransparent(Boolean transparent) {
-    this.transparent = transparent;
-  }
-
-  public Float getOpacity() {
-    return opacity;
-  }
-
-  public void setOpacity(Float opacity) {
-    this.opacity = opacity;
-  }
-
-  public Set<Map> getMaps() {
-    return maps;
-  }
-
-  public void setMaps(Set<Map> maps) {
-    this.maps = maps;
-  }
-
-  public Texture getTexture() {
-    return texture;
-  }
-
-  public void setTexture(Texture texture) {
-    this.texture = texture;
-  }
-
-  public Set<GeoMaterial> getGeoMaterials() {
-    return geoMaterials;
-  }
-
-  public void setGeoMaterials(Set<GeoMaterial> geoMaterials) {
-    this.geoMaterials = geoMaterials;
   }
 
   public Integer getDbgColor() {
@@ -194,44 +192,20 @@ public class Material implements Serializable {
     this.dbgName = dbgName;
   }
 
-  public String getColorAmbient() {
-    return colorAmbient;
-  }
-
-  public void setColorAmbient(String colorAmbient) {
-    this.colorAmbient = colorAmbient;
-  }
-
-  public String getColorDiffuse() {
-    return colorDiffuse;
-  }
-
-  public void setColorDiffuse(String colorDiffuse) {
-    this.colorDiffuse = colorDiffuse;
-  }
-
-  public String getColorSpecular() {
-    return colorSpecular;
-  }
-
-  public void setColorSpecular(String colorSpecular) {
-    this.colorSpecular = colorSpecular;
-  }
-
-  public String getMapDiffuse() {
-    return mapDiffuse;
-  }
-
-  public void setMapDiffuse(String mapDiffuse) {
-    this.mapDiffuse = mapDiffuse;
-  }
-
   public String getBlending() {
     return blending;
   }
 
   public void setBlending(String blending) {
     this.blending = blending;
+  }
+
+  public String getShading() {
+    return shading;
+  }
+
+  public void setShading(String shading) {
+    this.shading = shading;
   }
 
   public Boolean getDepthTest() {
@@ -250,36 +224,12 @@ public class Material implements Serializable {
     this.depthWrite = depthWrite;
   }
 
-  public String getMapDiffuseWrap() {
-    return mapDiffuseWrap;
+  public Boolean getTransparent() {
+    return transparent;
   }
 
-  public void setMapDiffuseWrap(String mapDiffuseWrap) {
-    this.mapDiffuseWrap = mapDiffuseWrap;
-  }
-
-  public String getShading() {
-    return shading;
-  }
-
-  public void setShading(String shading) {
-    this.shading = shading;
-  }
-
-  public Integer getSpecularCoef() {
-    return specularCoef;
-  }
-
-  public void setSpecularCoef(Integer specularCoef) {
-    this.specularCoef = specularCoef;
-  }
-
-  public Float getTransparency() {
-    return transparency;
-  }
-
-  public void setTransparency(Float transparency) {
-    this.transparency = transparency;
+  public void setTransparent(Boolean transparent) {
+    this.transparent = transparent;
   }
 
   public Boolean getVertexColors() {
@@ -290,20 +240,140 @@ public class Material implements Serializable {
     this.vertexColors = vertexColors;
   }
 
-  public Long getMaterialId() {
-    return materialId;
+  public String getColorDiffuse() {
+    return colorDiffuse;
   }
 
-  public void setMaterialId(Long materialId) {
-    this.materialId = materialId;
+  public void setColorDiffuse(String colorDiffuse) {
+    this.colorDiffuse = colorDiffuse;
   }
 
-  public Integer getSortIndex() {
-    return sortIndex;
+  public String getColorSpecular() {
+    return colorSpecular;
   }
 
-  public void setSortIndex(Integer sortIndex) {
-    this.sortIndex = sortIndex;
+  public void setColorSpecular(String colorSpecular) {
+    this.colorSpecular = colorSpecular;
+  }
+
+  public String getColorAmbient() {
+    return colorAmbient;
+  }
+
+  public void setColorAmbient(String colorAmbient) {
+    this.colorAmbient = colorAmbient;
+  }
+
+  public Float getTransparency() {
+    return transparency;
+  }
+
+  public void setTransparency(Float transparency) {
+    this.transparency = transparency;
+  }
+
+  public Integer getSpecularCoef() {
+    return specularCoef;
+  }
+
+  public void setSpecularCoef(Integer specularCoef) {
+    this.specularCoef = specularCoef;
+  }
+
+  public Float getOpacity() {
+    return opacity;
+  }
+
+  public void setOpacity(Float opacity) {
+    this.opacity = opacity;
+  }
+
+  public String getMapDiffuse() {
+    return mapDiffuse;
+  }
+
+  public void setMapDiffuse(String mapDiffuse) {
+    this.mapDiffuse = mapDiffuse;
+  }
+
+  public String getMapLight() {
+    return mapLight;
+  }
+
+  public void setMapLight(String mapLight) {
+    this.mapLight = mapLight;
+  }
+
+  public String getMapBump() {
+    return mapBump;
+  }
+
+  public void setMapBump(String mapBump) {
+    this.mapBump = mapBump;
+  }
+
+  public String getMapNormal() {
+    return mapNormal;
+  }
+
+  public void setMapNormal(String mapNormal) {
+    this.mapNormal = mapNormal;
+  }
+
+  public String getMapSpecular() {
+    return mapSpecular;
+  }
+
+  public void setMapSpecular(String mapSpecular) {
+    this.mapSpecular = mapSpecular;
+  }
+
+  public String getMapDiffuseWrap() {
+    return mapDiffuseWrap;
+  }
+
+  public void setMapDiffuseWrap(String mapDiffuseWrap) {
+    this.mapDiffuseWrap = mapDiffuseWrap;
+  }
+
+  public String getMapLightWrap() {
+    return mapLightWrap;
+  }
+
+  public void setMapLightWrap(String mapLightWrap) {
+    this.mapLightWrap = mapLightWrap;
+  }
+
+  public String getMapBumpWrap() {
+    return mapBumpWrap;
+  }
+
+  public void setMapBumpWrap(String mapBumpWrap) {
+    this.mapBumpWrap = mapBumpWrap;
+  }
+
+  public String getMapNormalWrap() {
+    return mapNormalWrap;
+  }
+
+  public void setMapNormalWrap(String mapNormalWrap) {
+    this.mapNormalWrap = mapNormalWrap;
+  }
+
+  public String getMapSpecularWrap() {
+    return mapSpecularWrap;
+  }
+
+  public void setMapSpecularWrap(String mapSpecularWrap) {
+    this.mapSpecularWrap = mapSpecularWrap;
+  }
+
+  public Float getMapBumpScale() {
+    return mapBumpScale;
+  }
+
+  public void setMapBumpScale(Float mapBumpScale) {
+    this.mapBumpScale = mapBumpScale;
   }
 
   public String getAttributes() {
@@ -336,6 +406,38 @@ public class Material implements Serializable {
 
   public void setFragmentShader(String fragmentShader) {
     this.fragmentShader = fragmentShader;
+  }
+
+  public Long getMaterialId() {
+    return materialId;
+  }
+
+  public void setMaterialId(Long materialId) {
+    this.materialId = materialId;
+  }
+
+  public Texture getTexture() {
+    return texture;
+  }
+
+  public void setTexture(Texture texture) {
+    this.texture = texture;
+  }
+
+  public Set<Map> getMaps() {
+    return maps;
+  }
+
+  public void setMaps(Set<Map> maps) {
+    this.maps = maps;
+  }
+
+  public Set<GeoMaterial> getGeoMaterials() {
+    return geoMaterials;
+  }
+
+  public void setGeoMaterials(Set<GeoMaterial> geoMaterials) {
+    this.geoMaterials = geoMaterials;
   }
 
   @Override
