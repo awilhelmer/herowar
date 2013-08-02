4178c7910c62086512f3ff0e64d5f02c42be2f7a
awilhelmer <awilhelmer@gmx.de>
2013-08-02 22:43:04 +0200

    prework for shadertest

---
+++ app/controllers/ShaderTest.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ShaderTest extends Controller {
+
+  public static Result index() {
+    return ok(views.html.game.shadertest.render("ShaderTest", null));
+  }
+}
